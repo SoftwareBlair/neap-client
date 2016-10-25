d2bc6bc961e5ac5eed8c937c33b6de9714f63c48
Brandon Blair <brandon.d.blair@gmail.com>
2016-10-25 14:45:50 -0600

    added login form

--- js/services.js
+++ js/services.js
@@ -4,27 +4,45 @@
 
   angular
     .module('myApp.services', [])
-    .service('coffeeService', coffeeService);
+    .service('coffeeService', coffeeService)
+    .service('userService', userService);
 
   coffeeService.$inject = ['$http'];
+  userService.$inject = ['$http'];
 
-  function coffeeService($http) {
+  function coffeeService ($http) {
     /*jshint validthis: true */
     const baseURL = 'https://serene-springs-41881.herokuapp.com/coffee/';
+
     this.getAllCoffee = function() {
       return $http.get(baseURL);
     };
-    this.getSingleCoffee = function(id) {
+
+    this.getSingleCoffee = function (id) {
       return $http.get(`${baseURL}${id}`);
     };
-    this.addCoffee = function(coffee) {
+
+    this.addCoffee = function (coffee) {
       return $http({
         method: 'POST',
         url: baseURL,
         data: coffee,
         headers: {'Content-Type': 'application/json'}
       });
-      // return $http.post(baseURL, coffee);
+    };
+  }
+
+  function userService ($http) {
+    /*jshint validthis: true */
+    const baseURL = 'http://localhost:8000/user/';
+
+    this.login = function (user) {
+      return $http({
+        method: 'POST',
+        url: baseURL + 'login',
+        data: user,
+        headers: {'Content-Type': 'application/json'}
+      });
     };
   }
 
